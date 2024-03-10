9635f467510a90bd447ea034da934376b7a51525
oluwaturheeb <oluwaturheeb@gmail.com>
2024-03-10 07:30:18 +0100

    code updated]

--- android/src/main/java/com/devtee/dialussd/DialUssdModule.java
+++ android/src/main/java/com/devtee/dialussd/DialUssdModule.java
@@ -2,28 +2,58 @@
 
 package com.devtee.dialussd;
 
+import androidx.annotation.NonNull;
+
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import com.facebook.react.module.annotations.ReactModule;
+// added
+import android.os.Handler;
+import android.os.Looper;
+import android.telephony.TelephonyManager;
+import android.content.Context;
 
+@ReactModule(name = DialUssdModule.NAME)
 public class DialUssdModule extends ReactContextBaseJavaModule {
+  public static final String NAME = "DialUssd";
+  private ReactApplicationContext reactContext;
 
-    private final ReactApplicationContext reactContext;
+  public DialUssdModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+    this.reactContext = getReactApplicationContext();
+  }
 
-    public DialUssdModule(ReactApplicationContext reactContext) {
-        super(reactContext);
-        this.reactContext = reactContext;
+  @Override
+  @NonNull
+  public String getName() {
+    return NAME;
+  }
+
+  @ReactMethod
+  public void dialUssd(String ussdCode, final Promise promise) {
+    TelephonyManager telephonyManager = (TelephonyManager) this.reactContext
+        .getSystemService(Context.TELEPHONY_SERVICE);
+    if (telephonyManager != null) {
+      telephonyManager.sendUssdRequest(ussdCode, new TelephonyManager.UssdResponseCallback() {
+        @Override
+        public void onReceiveUssdResponse(TelephonyManager telephonyManager, String request, CharSequence response) {
+          try {
+            super.onReceiveUssdResponse(telephonyManager, request, response.toString());
+            promise.resolve(response.toString());
+          } catch (Exception e) {
+            promise.reject("Can not perfom the action!");
+          }
+        }
+
+        @Override
+        public void onReceiveUssdResponseFailed(TelephonyManager telephonyManager, String request, int failureCode) {
+          super.onReceiveUssdResponseFailed(telephonyManager, request, failureCode);
+          promise.reject("Failed to send USSD code. Failure code: " + failureCode);
+        }
+      }, new Handler());
     }
-
-    @Override
-    public String getName() {
-        return "DialUssd";
-    }
-
-    @ReactMethod
-    public void sampleMethod(String stringArgument, int numberArgument, Callback callback) {
-        // TODO: Implement some actually useful functionality
-        callback.invoke("Received numberArgument: " + numberArgument + " stringArgument: " + stringArgument);
-    }
+  }
 }
--- example/App.tsx
+++ example/App.tsx
@@ -1,118 +1,60 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, {Component} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
+  PermissionsAndroid,
+  Platform,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
 } from 'react-native';
+import DialUssd from 'dialussd';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+export default class App extends Component<{}> {
+  state = {
+    status: 'starting',
+    message: '--',
   };
+  componentDidMount() {
+    (async () => {
+      const perm = await PermissionsAndroid.request(
+        'android.permission.CALL_PHONE',
+      );
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+      if (perm == 'granted') {
+        let call = await DialUssd?.dialUssd('*461*4#');
+        this.setState({
+          status: 'native callback received',
+          message: call,
+        });
+      }
+    })();
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>☆DialUssd example☆</Text>
+        <Text style={styles.instructions}>STATUS: {this.state.status}</Text>
+        <Text style={styles.welcome}>☆NATIVE CALLBACK MESSAGE☆</Text>
+        <Text style={styles.instructions}>{this.state.message}</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
   },
 });
-
-export default App;
